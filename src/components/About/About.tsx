a0eb728bb2d2d8407e0e5bae6d4907131671b44a
Daniel Brüggemann <github@barfooz.com>
2019-02-28 18:40:06 +0100

    Add Article component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,18 +1,26 @@
 import * as React from "react";
+import styled from "styled-components";
 
+import { Article } from "components/Article";
 import { Link } from "components/Link";
 
 import { SocialLink, Props } from "./types";
 
 export const About = ({ socialLinks }: Props): JSX.Element => (
   <>
-    <h2>Social</h2>
-    <ul>
-      {socialLinks.map((link: SocialLink) => (
-        <li key={link.url}>
-          <Link to={link.url}>{link.name}</Link>
-        </li>
-      ))}
-    </ul>
+    <Article>
+      <h2>Me</h2>
+      <p>{"Hej i'm Daniel Brüggemann, Freelance Software-Developer."}</p>
+    </Article>
+    <Article>
+      <h2>Social</h2>
+      <ul>
+        {socialLinks.map((link: SocialLink) => (
+          <li key={link.url}>
+            <Link to={link.url}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </Article>
   </>
 );
--- src/components/Vita.tsx
+++ src/components/Vita.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 
+import { Article } from "components/Article";
+
 export const Vita = (): JSX.Element => (
   <>
-    <p>Welcome to page 2</p>
-    <h2>Skills</h2>
-    <p>Welcome to page 2</p>
-    <h2>References</h2>
-    <p>Welcome to page 2</p>
+    <Article>
+      <p>Welcome to page 2</p>
+    </Article>
+    <Article>
+      <h2>Key Skills</h2>
+      <p>Welcome to page 2</p>
+    </Article>
+    <Article>
+      <h2>References</h2>
+      <p>Welcome to page 2</p>
+    </Article>
+    <Article>
+      <h2>Professional Experience</h2>
+      <p>Welcome to page 2</p>
+    </Article>
   </>
 );
