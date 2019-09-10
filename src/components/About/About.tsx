83653f1607565da0c9d0d1486252289b0bf46ed0
Daniel Brüggemann <github@barfooz.com>
2019-09-10 08:42:42 +0200

    Add image and more content to `/About` page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,21 +6,54 @@ import { Link } from "components/Link";
 
 import { SocialLink, Props } from "./types";
 
+const FlexArticle = styled(Article)`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+
+  @media (${props => props.theme.breakpoints.desktop}) {
+    flex-direction: row;
+  }
+`;
+
+const Image = styled.img`
+  border-radius: 50%;
+  border: 2px solid black;
+  display: block;
+  flex: 1;
+  height: auto;
+  width: 50%;
+  margin: 0 auto 2rem;
+
+  @media (${props => props.theme.breakpoints.desktop}) {
+    margin: 0 3rem 0 0;
+  }
+`;
+
+const Desciption = styled.div`
+  flex: 3;
+`;
+
 export const About = ({ socialLinks }: Props): JSX.Element => (
-  <>
-    <Article>
+  <FlexArticle>
+    <Image src="/images/me.jpg" alt="Me" />
+    <Desciption>
       <h2>Me</h2>
-      <p>{"Hej i'm Daniel Brüggemann, Freelance Software-Developer."}</p>
-    </Article>
-    <Article>
+      <p>
+        {
+          "Hi, i'm Daniel Brüggemann a Freelance Software developer from Nennhausen."
+        }
+      </p>
       <h2>Social</h2>
       <ul>
-        {socialLinks.map((link: SocialLink) => (
-          <li key={link.url}>
-            <Link to={link.url}>{link.name}</Link>
-          </li>
-        ))}
+        {socialLinks.map(
+          (link: SocialLink): JSX.Element => (
+            <li key={link.url}>
+              <Link to={link.url}>{link.name}</Link>
+            </li>
+          )
+        )}
       </ul>
-    </Article>
-  </>
+    </Desciption>
+  </FlexArticle>
 );
