1f23004623161c783462a15f8efdd4b28fdc5b46
Daniel Brüggemann <github@barfooz.com>
2019-11-28 14:44:01 +0100

    Update Skill component styling

--- src/components/Vita/Skill.tsx
+++ src/components/Vita/Skill.tsx
@@ -1,11 +1,26 @@
 import styled from "styled-components";
 
 export const Skill = styled.span`
-  background-color: ${props => props.theme.colors.grey};
-  border-radius: 0.5em;
-  box-shadow: -0.2rem 0.2rem 0px 0px ${props => props.theme.colors.primary};
+  background-color: ${props => props.theme.colors.black};
+  color: ${props => props.theme.colors.grey};
   display: inline-block;
   font-size: 0.85em;
   margin: 0.5rem;
-  padding: 0.5rem 1rem;
+  padding: 0.25rem 0.5rem;
+
+  &:before,
+  &:after {
+    color: ${props => props.theme.colors.primary};
+    display: inline-block;
+    font-weight: 700;
+  }
+
+  &:before {
+    content: "<";
+  }
+
+  &:after {
+    content: "/>";
+    margin-left: 0.5rem;
+  }
 `;
