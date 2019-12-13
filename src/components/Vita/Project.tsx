3d75183c5215a95b0eec556a7c34992104fae1ea
Daniel Brüggemann <github@barfooz.com>
2019-12-13 19:12:13 +0100

    Remove opacity from Project image

--- src/components/Vita/Project.tsx
+++ src/components/Vita/Project.tsx
@@ -6,7 +6,6 @@ import { Project as Props } from "./types";
 const Img = styled.img<{ size: number }>`
   height: auto;
   width: ${props => 15 * props.size}%;
-  opacity: 0.75;
   margin: 1rem;
 
   @media (${props => props.theme.breakpoints.desktop}) {
